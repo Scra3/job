63bd1b8069dbbeab77d9e34d76d999cd7bf60b0b
alban bertolini <alban@sogilis.com>
2021-09-27 11:33:32 +0200

    level2

--- backend/lib/rental.rb
+++ backend/lib/rental.rb
@@ -8,7 +8,7 @@ class Rental
   end
 
   def price
-    time_price + distance_price
+    (time_price + distance_price).to_i
   end
 
   def as_json
@@ -21,7 +21,18 @@ class Rental
   private
 
   def time_price
-    @driver.count_period * @car.price_per_day
+    (1..@driver.count_period).map do |day_index|
+      if day_index > 10
+        next(@car.price_per_day - @car.price_per_day * 0.5)
+      end
+      if day_index > 4
+        next(@car.price_per_day - @car.price_per_day * 0.3)
+      end
+      if day_index > 1
+        next(@car.price_per_day - @car.price_per_day * 0.1)
+      end
+      @car.price_per_day
+    end.sum
   end
 
   def distance_price
--- backend/spec/rental_spec.rb
+++ backend/spec/rental_spec.rb
@@ -5,21 +5,58 @@ require 'date'
 
 RSpec.describe Rental do
   let(:three_days_period) {Date.today..Date.today + 2}
-  let(:driver) {Driver.new(three_days_period, 20)}
+  let(:a_driver) {Driver.new(three_days_period, 20)}
   let(:car) {Car.new(1, 10, 15)}
 
   describe '#price' do
-    it "returns the price by given a driver and a car" do
-      rental = Rental.new(1, car, driver)
+    let(:one_day_reduction) { car.price_per_day - car.price_per_day * 0.1 }
+    let(:four_days_reduction) { car.price_per_day - car.price_per_day * 0.3 }
+    let(:ten_days_reduction) {  car.price_per_day - car.price_per_day * 0.5 }
 
-      expected_price = car.price_per_day * driver.count_period + car.price_per_km * driver.approximate_distance
-      expect(rental.price).to eq expected_price
+    context 'price does not decrease when the period is one day' do
+      it "returns the price by given a driver and a car" do
+        driver = Driver.new(Date.today..Date.today, 20)
+        rental = Rental.new(1, car, driver)
+
+        expected_price = car.price_per_day * driver.count_period + car.price_per_km * driver.approximate_distance
+        expect(rental.price).to eq expected_price
+      end
+    end
+
+    context 'price per day decreases by 10% after 1 day' do
+      it "returns the price by given a driver and a car" do
+        driver = Driver.new(Date.today..Date.today + 1, 20)
+        rental = Rental.new(1, car, driver)
+
+        expected_price = car.price_per_day + one_day_reduction + car.price_per_km * driver.approximate_distance
+        expect(rental.price).to eq expected_price
+      end
+    end
+
+    context 'price per day decreases by 30% after 4 days' do
+      it "returns the price by given a driver and a car" do
+        driver = Driver.new(Date.today..Date.today + 4, 20)
+        rental = Rental.new(1, car, driver)
+
+        expected_price = car.price_per_day + one_day_reduction * 3 + four_days_reduction + car.price_per_km * driver.approximate_distance
+        expect(rental.price).to eq expected_price
+      end
+    end
+
+    context 'price per day decreases by 50% after 10 days' do
+      it "returns the price by given a driver and a car" do
+        driver = Driver.new(Date.today..Date.today + 10, 20)
+        rental = Rental.new(1, car, driver)
+
+        expected_price = car.price_per_day + one_day_reduction * 3 + four_days_reduction * 6 + ten_days_reduction + car.price_per_km * driver.approximate_distance
+        expect(rental.price).to eq expected_price
+      end
     end
   end
 
   describe '#as_json' do
     it "returns the price and the id attributes as json data" do
-      rental = Rental.new(1, car, driver)
+      rental = Rental.new(1, car, a_driver)
 
       expect(rental.as_json).to eq({id: 1, price: rental.price})
     end
